130a9c02f4a8740ddd2260de59fc6ed93bbb17e1
Karen <karen.zoroglyan@mail.ru>
2022-01-02 20:48:11 +0400

    created TodoItem component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,12 @@
+import TodoItem from "./TodoItem";
+
 function TodoList({todos}) {
     return (
         <div>
             {
                 todos.map((todo) => {
                     return (
-                        <div key={todo.id}>{todo.text}</div>
+                        <TodoItem key={todo.id} todo={todo}/>
                     )
                 })
             }
